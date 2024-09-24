0825bb88a9ce0c1433aa9dd994d06c6e2273f4dd
Egbadon Victor <51365624+egbadon-victor@users.noreply.github.com>
2024-09-24 14:20:45 +0000

    reorganizes admin site and moves choices inline

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    question = "question"
+    text = "choice_text"
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+    exclude=["votes"]
+
+class QuestionAdmin(admin.ModelAdmin):
+    question = "question_text"
+    date_published = "pub_date"   
+    inlines=[ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
